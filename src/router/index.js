1743b558ac69d2a4a76de970162bce1e1fa2ca9a
CopilotLaLaLa <1060770125@qq.com>
2022-06-02 19:39:32 +0800

    加入页面跳转滚动到顶部逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -35,7 +35,14 @@ const routes = [
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 export default router
